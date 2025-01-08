a4a1c154de71a63ca8c51c856a0c4041e3eaeda0
Sebastian Susunaga <susunaga414.ss@gmail.com>
2025-01-07 20:51:27 -0500

    Clase 5 de java

---
+++ Operaciones.java
@@ -0,0 +1,14 @@
+public class Operaciones{
+    public static void main (String args[]){
+
+        int num_1 = 4;
+        int num_2 = 4;
+        int resultado = 0;
+
+        resultado = num_1 + num_2 / 2;
+        System.out.println("El resultado 1 es: " + resultado);
+
+        resultado = (num_1 + num_2) / 2;
+        System.out.println("El resultado 2 es: " + resultado);
+    }
+}
