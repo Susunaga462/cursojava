78f83d867f14868bcb32f58982850931ddf0aaab
Sebastian Susunaga <susunaga414.ss@gmail.com>
2025-02-12 19:23:40 -0500

    curso java 11

---
+++ Switch.java
@@ -0,0 +1,27 @@
+public class Switch{
+    public static void main(String args[]){
+
+        int num1 = 5, num2 = 3, resultado = 0;
+        int parametro = 10;
+        switch(parametro){
+            case 1: resultado = num1 + num2 ;
+            System.out.println("El resultado de la suma es: " + resultado);
+            break;
+
+            case 2: resultado = num1 - num2;
+            System.out.println("El resultado de la resta es: " + resultado);
+            break;
+
+            case 3: resultado = num1 * num2;
+            System.out.println("El resultado de la multiplicacion es: " + resultado);
+            break;
+
+            case 4: resultado = num1 / num2;
+            System.out.println("El resultado de la division es : " + resultado);
+            break;
+
+            default: System.out.println("Error, la opcion no existe");
+            break;
+        }
+    }
+}
